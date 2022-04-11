9fb10b96fcd6855c8577e55c88ac9d396cd23c0d
Lince Mathew <linz07m@gmail.com>
2022-04-11 09:12:42 +0530

    fix:trigger condition handler WIP

--- return_book.py
+++ return_book.py
@@ -1,13 +1,22 @@
 from tkinter import *
+from tkinter import messagebox
+import datetime
+from connect_db import connect
+import pymysql
 def return_books(home,return_book):
     return_book.configure(background='black')
     Label(return_book,text='Return Book',bg='black', fg='white', font=('Courier',35)).pack(pady=15)
-    Label(return_book,text='Book Id',bg='black', fg='white', font=('Courier',25)).pack(pady=5)
-    Text(return_book,height = 1,width = 30,font=('Courier',25)).pack(pady=5)
-    Label(return_book,text='Student Id',bg='black', fg='white', font=('Courier',25)).pack(pady=5)
+    Label(return_book,text='Issue Id',bg='black', fg='white', font=('Courier',25)).pack(pady=5)
     Text(return_book,height = 1,width = 30,font=('Courier',25)).pack(pady=5)
     return_book.pack(expand=1,fill = X)
     Button(return_book,text='Submit',command=lambda:returnbook(),bg='black', fg='white', font=('Courier',25)).pack(pady=12)
     home.pack_forget()
-def returnbook():
-    return None
+def returnbook(issue_id):
+    book_id=book_id.get(1.0, "end-1c")
+    today = datetime.date.today()
+    returned_date=str(today)
+    try:
+        query="update Issue set returned_date="+str(returned_date)
+        messagebox.showinfo("Success","Book returned")
+    except(pymysql.Error, pymysql.Warning) as e:
+        messagebox.showinfo("Error",str(e))
